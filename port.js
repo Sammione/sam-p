456210cc2956f498549c3b5e4ea91aa06c49c5c0
Sammione <108240614+Sammione@users.noreply.github.com>
2023-12-19 15:02:21 +0100

    Update port.js

--- port.js
+++ port.js
@@ -37,18 +37,33 @@ scrollTrigger('.raw', {
   rootMargin: '-20px',
 });
 
-const btngrp = document.querySelectorAll('.btn');
-const arrpdf = ['https://onedrive.live.com/embed?resid=D077F1DD850104C7%21669&authkey=!AOJ1x1sBlKl_rRY&em=2', 
-'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21668&authkey=!ALGmtieQ4s7cUio&em=2', 
-'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21667&authkey=!APVSlbzgtIika7k&em=2',
- 'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21670&authkey=!AOfmRKkrMvPHq7Y&em=2', 
+let arrpdf = ['https://onedrive.live.com/embed?resid=D077F1DD850104C7%21694&authkey=!AJKwV55YJ_I0y-0&em=2','https://onedrive.live.com/embed?resid=D077F1DD850104C7%21693&authkey=!AAwyuKL_2pDZ4eg&em=2','https://onedrive.live.com/embed?resid=D077F1DD850104C7%21692&authkey=!AEWFwwH1G0z9fms&em=2','https://onedrive.live.com/embed?resid=D077F1DD850104C7%21669&authkey=!AOJ1x1sBlKl_rRY&em=2'
+,'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21667&authkey=!APVSlbzgtIika7k&em=2', 
  'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21671&authkey=!ABJoaxTZdKQt_VA&em=2',
   'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21672&authkey=!AFDp3hdOBP70C6E&em=2', 
-'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21675&authkey=!ALiUx8v59cY2ifk&em=2', 
 'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21673&authkey=!AOEtOQOmYbP90rw&em=2', 
 'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21674&authkey=!ALXvCoY4OAdhyUU&em=2', 
 'https://onedrive.live.com/embed?resid=D077F1DD850104C7%21676&authkey=!AAKyYEWSPwjN_RA&em=2'];
 
+let butty = ['Business Optimization', 'Customer Preference & Price Segmentation', 'Upsell Opportunity Analysis', 'Covid-19 Sentiment Analysis', 
+'EDA on VG-sales', 'Nigeria"s Economy using LSTM', 'Nigeria Economy Indicator Using Times Series',
+'Stock Prediction', 'Recommender System', 'Phone Review'];
+
+const prol = document.querySelector('.prol');
+
+butty.forEach((but)=>{
+    let rest = `
+        <div class="project-data">
+            <div class="project-topic">
+                <h3>${but}</h3>
+            </div>
+                <button class="btn anc">Preview</button>
+        </div>`;
+
+    prol.innerHTML += rest;
+})
+
+const btngrp = document.querySelectorAll('.btn');
 const embed = document.querySelector('.pdf');
 const viewer = document.querySelector('.pdf-view');
 
@@ -73,4 +88,3 @@ function showMenu(){
         menu.classList.remove('active');
     }
 }
-
